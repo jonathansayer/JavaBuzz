28b798df9280077d7a580a6120472813ae7d478c
Elin <elinnet@gmail.com>
2015-07-06 15:25:59 +0100

    refactor of javabuzz.js.  all tests pass

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -2,21 +2,24 @@
  var Javabuzz = function (){
 
   Javabuzz.prototype.isDivisibleByThree = function(number) {
-    return number % 3 === 0;
+    return this._isDivisbleBy(number,3);
   };
 
   Javabuzz.prototype.isDivisibleByFive = function(number) {
-    return number % 5 === 0;
+    return this._isDivisbleBy(number,5);
   };
 
   Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-    return number % 15 === 0;
+    return this._isDivisbleBy(number,15);
   };
 
 
+  Javabuzz.prototype._isDivisbleBy = function(number,divisor){
+    return (number % divisor === 0);
+  };
+
 
 };
-
 
 
 
